b479d262cc291d16de168d1545f86b99998ab678
Bals-0010 <bala.bhar920@gmail.com>
2021-02-13 22:59:51 +0530

    added 3 files

--- Edabit/Friday the 13th.py
+++ Edabit/Friday the 13th.py
@@ -11,3 +11,14 @@ January will be given as 1, February as 2, etc ...
 Check Resources for some helpful tutorials on Python's datetime module.
 """
 
+import calendar
+
+def has_friday_13(month, year):     
+    dayNumber = calendar.weekday(year, month, 13) 
+    days =["Monday", "Tuesday", "Wednesday", "Thursday", 
+                         "Friday", "Saturday", "Sunday"] 
+    return True if (days[dayNumber]) =="Friday" else False
+
+print(has_friday_13(3, 2020))
+print(has_friday_13(10, 2017))
+print(has_friday_13(1, 1985))
